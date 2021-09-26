c2816e4334bdaedcdb19d27190eee57ebcbb74c6
Andrei <KAKTYC.DAS@gmail.com>
2021-09-26 16:38:12 +0200

    refactored running, added funcs

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import requests
+
 import settings
-from settings import my_token, root_url, updates_endpoint, message_endpoint, ok_codes, hello_message, message_unknown_country, available_currency_countries
 from currencies import today_currency_by_abbr, currency_message_to_user
+from settings import root_url, updates_endpoint, message_endpoint, ok_codes, hello_message, message_unknown_country, \
+    available_currency_countries
+from weather import create_weather_message
 
 
-def get_updates(token):
+def get_updates(token: str) -> dict:
     updates_url = f"{root_url}{token}{updates_endpoint}"
     res = requests.get(updates_url)
     if res.status_code in ok_codes:
@@ -14,7 +17,7 @@ def get_updates(token):
         print(f"Неудача с запросом: статус {res.status_code}")
 
 
-def send_message(chat_id, text_message, token):
+def send_message(chat_id: str, text_message: str, token: str) -> bool:
     send_message_url = f"{root_url}{token}{message_endpoint}"
     res = requests.post(send_message_url, data={"chat_id": chat_id, "text": text_message})
     if res.status_code in ok_codes:
@@ -23,20 +26,21 @@ def send_message(chat_id, text_message, token):
         print(f"Не удалось послать сообщение - ошибка с кодом {res.status_code}")
 
 
-def pooling(token):
+def pooling(token: str) -> None:
     last_message_number = 0
     while True:
         updates = get_updates(token)
         if updates["result"]:
             message_id = updates["result"][-1]["message"]["message_id"]
-            chat_id = updates["result"][-1]["message"]["chat"]["id"] 
+            chat_id = updates["result"][-1]["message"]["chat"]["id"]
             last_message_text = updates["result"][-1]["message"]["text"]
 
             if message_id > last_message_number:
-            process_message(chat_id, message_text, token)
-            last_message_number = message_id
+                process_message(chat_id, last_message_text, token)
+                last_message_number = message_id
 
-def process_message(chat_id, message_text, token):
+
+def process_message(chat_id: str, message_text: str, token: str) -> None:
     if "/start" in message_text:
         send_message(chat_id, hello_message, token)
     if "/курс" in message_text:
@@ -50,10 +54,12 @@ def process_message(chat_id, message_text, token):
                 send_message(chat_id, message_unknown_country, token)
         else:
             print("Убедитесь, что собщение составленно в верном формате: например '/курс USD' ")
-    
+
     if "/country" in message_text:
         if message_text[-2:] in available_currency_countries:
             settings.user_country = message_text[-2:]
-
-
-pooling(my_token)
+    if "/weather" in message_text:
+        city = message_text[9::]
+        weather_message = create_weather_message(city)
+        if weather_message:
+            send_message(chat_id, weather_message, token)
