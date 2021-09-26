edf32237c90612d3ed8a334f24bc3c7efd3380d2
Andrei <KAKTYC.DAS@gmail.com>
2021-09-26 12:04:31 +0200

    Added pulling and echo

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,12 +6,12 @@ get_me = "/getMe"
 updates_endpoint = "/getUpdates"
 message_endpoint = "/sendMessage"
 
+
 OK_CODES = (200, 201, 202, 203, 204, 205)
 
 
 def get_updates(token):
     updates_url = f"{root_url}{token}{updates_endpoint}"
-
     res = requests.get(updates_url)
     if res.status_code in OK_CODES:
         result = res.json()
@@ -29,7 +29,17 @@ def send_message(chat_id, text_message, token):
         print(f"Не удалось послать сообщение - ошибка с кодом {res.status_code}")
 
 
-updates = get_updates(my_token)
-chat_id = updates["result"][-1]["message"]["chat"]["id"]
-message = "Hello!"
-send_message(chat_id, message, my_token)
+def echo_message(token):
+    updates = get_updates(token)
+    chat_id = updates["result"][-1]["message"]["chat"]["id"]  
+    last_message_text = updates["result"][-1]["message"]["text"]
+    send_message(chat_id, last_message_text, token)
+
+last_message_number = 0
+while True:
+    updates = get_updates(my_token)
+    message_id = updates["result"][-1]["message"]["message_id"]
+
+    if message_id > last_message_number:
+        echo_message(my_token)
+        last_message_number = message_id
