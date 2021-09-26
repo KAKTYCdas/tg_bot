da93e1f30e201d3673eeee05b439676202db7502
Andrei <KAKTYC.DAS@gmail.com>
2021-09-26 19:07:53 +0200

    Refactored bot with classes(OOP), added packages

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from bot import pooling
-from settings import telegram_bot_token
+from logic.bot import TelegramBot
+from logic.settings import telegram_bot_token
 
 if __name__ == '__main__':
-	pooling(telegram_bot_token)
+	bot = TelegramBot(token=telegram_bot_token, country="") 
+	bot.pooling()
